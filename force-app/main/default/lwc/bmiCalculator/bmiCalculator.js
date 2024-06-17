0042824be8abe0d4c5f43bf31e340d23ff810f7e
axcelespinal <hyrulelink8zelda@gmail.com>
2024-06-17 18:31:17 -0400

    Added template logic for unit systems

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -1,10 +1,27 @@
+import { validateWaveTemplate } from 'lightning/analyticsWaveApi';
 import { LightningElement } from 'lwc';
 
 export default class BmiCalculator extends LightningElement {
-    get options() {
+    inputSystem;
+    isImperial;
+
+    get genders() {
         return [
             { label: 'Male', value: 'male' },
             { label: 'Female', value: 'female' },
         ];
+    }
+
+    get systems() {
+        return [
+            {label: 'Imperial', value: 'Imperial'},
+            {label: 'Metric', value: 'Metric'},
+        ];
+    }
+
+    handleSystemChange(event) {
+        this.inputSystem = event.target.value;
+        this.inputSystem == 'Imperial'? this.isImperial = true: this.isImperial=false;
+        console.log(this.isImperial);
     }
 }
